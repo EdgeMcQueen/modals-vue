6d2734333831e200b3545078d81c612e245d3f79
sardormutalov <sardor.mutalov@gmail.com>
2020-08-28 11:06:15 +0300

    modal-hw: refactor project

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="buttons-container">
+    <button class="btn btnPrimary" @click="modalTemplateShow = !modalTemplateShow">Show modal temlate</button>
+    <button class="btn btnPrimary" @click="modalExemplesShow = !modalExemplesShow">Show modal exemple</button>
+    <button class="btn btnPrimary" @click="modalExemplesShow = !modalExemplesShow">Show modal exemple</button>
+    </div>
+    <!-- modal template -->
+    <ModalTemplate
+      title="Modal template title"
+      v-show="modalTemplateShow"
+      @close="modalTemplateShow = false">
+
+      <div slot="body">
+        <p>Some text</p>
+        <button class="btn btnPrimary" @click="modalTemplateShow = !modalTemplateShow">Done!</button>
+      </div>
+    </ModalTemplate>
+    <!-- modal exemles -->
+    <ModalExemples
+      :modalFirst="modalFirst"
+      :modalSecond="modalSecond"
+      :modalValidate="modalValidate"
+      v-show="modalExemplesShow">
+
+    </ModalExemples>
+  </div>
+</template>
+
+
+<script>
+import ModalTemplate from '@/components/Modal/UI/ModalTemplate.vue';
+import ModalExemples from '@/components/Modal-exemples/ModalExemples.vue';
+
+export default {
+  components: {
+    ModalExemples,
+    ModalTemplate
+  },
+  data() {
+    return {
+      modalTemplateShow: false,
+      modalExemplesShow: false,
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: ''
+      },
+      modalValidate: false
+    }
+  },
+  methods: {
+    submitSecondForm () {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email
+      })
+      this.modalSecond.name = ''
+      this.modalSecond.email = ''
+      this.modalSecond.show = false
+
+    }
+  },
+
+}
+</script>
+<style lang="scss">
+  .buttons-container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/Modal-exemples/ModalExemples.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="container">
+         <!-- first modal -->
+        <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
+        <ModalTemplate
+        title="Firt modal"
+        v-show="modalFirst"
+        @close="modalFirst = false">
+
+        <div slot="body">
+            <p>some text</p>
+            <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well done!</button>
+        </div>
+        </ModalTemplate>
+
+        <!-- second modal -->
+        <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show form modal</button>
+        <ModalTemplate
+        title="Modal with form"
+        v-show="modalSecond.show"
+        @close="modalSecond.show = false">
+
+        <div slot="body">
+            <form @submit.prevent="submitSecondForm">
+            <label>Name:</label>
+                <input type="text" v-model="modalSecond.name" required>
+            <label>Email:</label>
+                <input type="email" v-model="modalSecond.email" required>
+            <button class="btn btnPrimary">Submit</button>
+            </form>
+        </div>
+        </ModalTemplate>
+
+        <!-- modal with validate -->
+        <button class="btn btnPrimary" @click="validModal = !validModal">Show modal with form + validate</button>
+        <ModalValidate v-show="validModal" @close="validModal = false"/>
+    </div>
+</template>
+
+<script>
+import ModalTemplate from '../Modal/UI/ModalTemplate.vue';
+import ModalValidate from './ModalValidate.vue';
+
+export default {
+  components: {
+    ModalValidate,
+    ModalTemplate,
+  },
+  props: {
+      modalFirst: {
+          type: Boolean,
+          required: true
+      },
+      modalSecond: {
+          type: Object,
+          required: true
+      },
+      modalValidate: {
+          type: Boolean,
+          required: true
+      }
+  },
+  data() {
+      return {
+          validModal: this.modalValidate
+      }
+  },
+}
+</script>
+<style lang="scss">
+    .container {
+        width: 100%;
+        padding: 30px 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+</style>
